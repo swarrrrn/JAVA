8ea307894c0e8f11bc0dcd32e673ca95268ca6e2
Swarn Kumar <swarnkumar.101@gmail.com>
2025-07-02 11:24:18 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+            int max1 = Integer.MIN_VALUE;
+            int max2 = Integer.MIN_VALUE;
+            for(int k : a){
+                if(k>max1){
+                    max2=max1;
+                    max1=k;
+                }
+                else if(k>max2){
+                    max2=k;
+                }
+            }
+            System.out.println(max1+max2);
+        }
+        
+    }
+}
